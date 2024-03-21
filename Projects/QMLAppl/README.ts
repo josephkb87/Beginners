ecb4d33706ad2e06e2ad8218df122c8ab1fbcbae
@josephkb87 <73903994+josephkb87@users.noreply.github.com>
2024-03-21 09:16:44 +0200

    Update cargo.lock

--- Projects/QMLAppl/README.ts
+++ Projects/QMLAppl/README.ts
@@ -1,1 +1,3 @@
-QML is a user interface specification and programming language. It allows developers and designers alike to create highly performant, fluidly animated and visually appealing applications. QML offers a highly readable, declarative, JSON-like syntax with support for imperative JavaScript expressions combined with dynamic property bindings.
+#### QML is a user interface specification and programming language. It allows developers and designers alike to create highly performant, fluidly animated and visually appealing applications.
+
+### QML offers a highly readable, declarative, JSON-like syntax with support for imperative JavaScript expressions combined with dynamic property bindings.
