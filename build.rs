859aeb48646d761d82bbed0072dc9887bfb1dfac
josephkb87 <kiyinijoseph@gmail.com>
2024-09-15 03:36:00 +0300

    Chore:Resolve rebase conflicts

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
     // Example custom build script.
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo::rerun-if-changed=src/hello.c");
+    println!("cargo::rerun-if-changed=src/main.rs");
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
         .file("src/main.rs")
-        .compile("main.rs);
+        .compile("main.rs");
 }
