de86bdd28d8fbc8345b06785fc03efee49c85604
josephkb87 <kiyinijoseph@gmail.com>
2024-09-15 03:19:50 +0300

    Chore:Resolve rebase conflicts

--- build.rs
+++ build.rs
@@ -4,6 +4,6 @@ fn main() {
     println!("cargo::rerun-if-changed=src/hello.c");
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-        .file("src/hello.c")
-        .compile("hello");
+        .file("src/main.rs")
+        .compile("main.rs);
 }
