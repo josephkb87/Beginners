9a5c5401a543a55d86d633687bb8e1dfa4ed7528
josephkb87 <kiyinijoseph@gmail.com>
2024-09-18 00:10:20 +0300

    Chore:Add precommit

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
     // Example custom build script.
 fn main() {
+
+    use std::collections::HashMap; // HashMap has been imported.
+let map: HashMap<u32, u32> = HashMap::new(); // So it can be used!
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo::rerun-if-changed=src/main.rs");
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-    use std::collections::HashMap; // HashMap has been imported.
-let map: HashMap<u32, u32> = HashMap::new(); // So it can be used!
         .file("src/main.rs")
         .compile("main.rs");
 }
