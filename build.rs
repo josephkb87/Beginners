840b2c39492303160555476e6020e99e6fcd81c4
josephkb87 <kiyinijoseph@gmail.com>
2024-09-18 00:08:51 +0300

    Chore:Add precommit

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ fn main() {
     println!("cargo::rerun-if-changed=src/main.rs");
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
+    use std::collections::HashMap; // HashMap has been imported.
+let map: HashMap<u32, u32> = HashMap::new(); // So it can be used!
         .file("src/main.rs")
         .compile("main.rs");
 }
