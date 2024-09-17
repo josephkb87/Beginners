437fb355038b005a53874e3b13a8165f587d4a28
josephkb87 <kiyinijoseph@gmail.com>
2024-09-17 23:30:54 +0300

    Chore:Add precommit

--- Projects/QMLAppl/README.ts
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-=======
-## QML is a user interface specification and programming language.
-    It allows developers and designers alike to create highly performant, fluidly animated and visually appealing applications.
-
-## QML offers a highly readable, declarative, JSON-like syntax with support for imperative JavaScript expressions combined with dynamic property bindings.
->>>>>>> refs/remotes/origin/master
